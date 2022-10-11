d62394c754ee3c0d2a6bb437209c4528af38460f
Hardik Agarwal <hardikagarwal812004@gmail.com>
2022-10-11 14:25:25 +0530

    11 oct

---
+++ day 15/gui_app.py
@@ -0,0 +1,62 @@
+# GUI - Graphical User Interaface
+
+# Libraries
+#####################
+# 1. Tkinter
+# 2. PyQT
+# 3. Turtle
+
+import tkinter as ttk
+
+app = ttk.Tk()
+app.title("My Calculator")
+app.geometry('600x400')
+# msg = ttk.Variable(app)
+# print(msg.get())
+# msg.set("Empty")
+# print(msg.get())
+ttk.Label(app,text = 'My Calculator',font=('Arial',25)).place(x=50,y=50)
+# ttk.Label(app,textvariable=msg).place(x=100,y=70)
+# def abc():
+#     print("WOW")
+#     msg.set("Jo tera mann kare")
+# ttk.Button(app,text = 'Isko Click Kardo',command=abc,font=("Arial",20)).place(x=100,y=100)
+# # ttk.Button(app,text="Yeh wala bhi hai",command=lambda:print("Wow")).place(x=100,y=130)
+# ttk.Button(app,text="Yeh wala bhi hai",command=lambda:msg.set("Pata Nahi"),font=("Arial",20)).place(x=150,y=200)
+
+
+################# CODE FOR CALCULATOR ########################
+
+f1=ttk.Variable(app)
+f1.set('0')
+f2=ttk.Variable(app)
+f2.set('0')
+result = ttk.Variable(app)
+
+
+ttk.Entry(app, textvariable=f1,font=('Arial',22)).place(x=50,y=200)
+ttk.Entry(app, textvariable=f2,font=('Arial',22)).place(x=150,y=200)
+ttk.Label(app,text="Result").place(x=100,y=300)
+ttk.Label(app,textvariable=result,font=('Arial',22)).place(x=100,y=330)
+
+def calci(op):
+    print("I will calculate")
+    result.set(eval(f1.get()+op+f2.get()))
+
+ttk.Button(app,text='+',command=lambda:calci('+'),font=('Arial',22)).place(x=50,y=240)
+ttk.Button(app,text='-',command=lambda:calci('-'),font=('Arial',22)).place(x=100,y=240)
+ttk.Button(app,text='*',command=lambda:calci('*'),font=('Arial',22)).place(x=150,y=240)
+ttk.Button(app,text='/',command=lambda:calci('/'),font=('Arial',22)).place(x=200,y=240)
+
+box=ttk.Listbox(app,height=5,fg='red',activestyle='dotbox')
+box.insert(1,'Sample1')
+box.insert(2,'Sample2')
+box.insert(3,'Sample3')
+
+box.place(x=350,y=40)
+def show():
+    print(box.get(box.curselection()))
+ttk.Button(app,text='show',command=show).place(x=350,y=250)
+
+
+app.mainloop()
